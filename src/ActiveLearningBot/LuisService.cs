a09bc0d6737255a103ccfca00edfabfd79d67cc5
André Secco <andreluizsecco@yahoo.com.br>
2018-06-25 19:18:54 -0300

    Update package Cognitive.LUIS.Programmatic

--- src/ActiveLearningBot/LuisService.cs
+++ src/ActiveLearningBot/LuisService.cs
@@ -1,8 +1,6 @@
 ﻿using Cognitive.LUIS.Programmatic;
 using Cognitive.LUIS.Programmatic.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ActiveLearningBot
@@ -19,24 +17,22 @@ namespace ActiveLearningBot
 
         public async Task Learn(string message, string intent)
         {
-            IEnumerable<Training> trainingList;
-            var client = new LuisProgClient("{YourSubscriptionKey}", Location.WestUS);
-            var app = await client.GetAppByNameAsync("Hotel");
-            await client.AddExampleAsync(app.Id, app.Endpoints.Production.VersionId, new Example
+            using (var client = new LuisProgClient("{YourSubscriptionKey}", Regions.WestUS))
             {
-                Text = message,
-                IntentName = intent
-            });
+                var app = await client.GetAppByNameAsync("Hotel");
+                await client.AddExampleAsync(app.Id, app.Endpoints.Production.VersionId, new Example
+                {
+                    Text = message,
+                    IntentName = intent
+                });
 
-            await client.TrainAsync(app.Id, app.Endpoints.Production.VersionId);
-            do
-            {
-                trainingList = await client.GetTrainingStatusListAsync(app.Id, app.Endpoints.Production.VersionId);
+                var trainingDetails = await client.TrainAndGetFinalStatusAsync(app.Id, app.Endpoints.Production.VersionId);
+                if (trainingDetails.Status.Equals("Success"))
+                {
+                    await client.PublishAsync(app.Id, app.Endpoints.Production.VersionId, false, "westus");
+                    await client.PublishAsync(app.Id, app.Endpoints.Production.VersionId, false, "westcentralus");
+                }
             }
-            while (!trainingList.All(x => x.Details.Status.Equals("Success")));
-
-            await client.PublishAsync(app.Id, app.Endpoints.Production.VersionId, false, "westus");
-            await client.PublishAsync(app.Id, app.Endpoints.Production.VersionId, false, "westcentralus");
         }
     }
 }
